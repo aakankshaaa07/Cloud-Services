6bb6d23907385c37efd07c9de1256c08f49cbf0d
Aakanksha Mantri <aakanksha.mantri07@gmail.com>
2023-01-10 18:30:30 +0530

    tailwind

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Support from './components/Support';
+import AllinOne from './components/AllinOne';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Support/>
+      <AllinOne/>
+      <Pricing/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
